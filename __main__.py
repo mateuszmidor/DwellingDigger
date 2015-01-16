700a05301bcd7aec70e2af411d252b739d7534e8
mateusz <kontakt@mateuszmidor.com>
2015-01-17 00:11:57 +0100

    Established directory structure. Added demo generating web page locally, showing predefined points on map

---
+++ __main__.py
@@ -0,0 +1,13 @@
+'''
+Created on 16-01-2015
+
+@author: mateusz
+'''
+import DesktopMain
+
+'''
+This is for manual testing purposes
+Will generate a web page with predefined points on map
+'''
+if __name__ == '__main__':
+    DesktopMain.demoRun()
---
+++ src/LightWebFramework/__init__.py
@@ -0,0 +1,1 @@
+from WebPageTemplate import WebPageTemplate
