31e0df7280a658e2d6d16e4858da4968d3ce0dc5
mateusz <kontakt@mateuszmidor.com>
2015-01-18 23:12:04 +0100

    Added testAllParamsSupplied test to test/ofers/gumtree/offer_querry_test.py

--- test/offers/gumtree/offer_querry_test.py
+++ test/offers/gumtree/offer_querry_test.py
@@ -19,8 +19,18 @@ class OfferQuerryTest(unittest.TestCase):
             pass
         except:
             self.fail("Empty 'city' param should cause ValueError exception but caused some other exception")
-
-
+            
+    def testAllParamsSupplied(self):
+        EXPECTED_QUERRY_STRING = 'http://www.gumtree.pl/fp-mieszkania-i-domy-do-wynajecia/Krakow/Nowa%20Huta/c9008l3200208?A_AreaInMeters_max=70&A_AreaInMeters_min=30&A_ForRentBy=ownr&A_NumberRooms=10&AdType=2&isSearchForm=true&maxPrice=1000&maxPriceBackend=200000&minPrice=500&minPriceBackend=100000'
+        querry = OfferQuerry.compose(city="Krakow",
+                                       whereabouts="Nowa Huta",
+                                       numRooms="1",
+                                       minPrice="500",
+                                       maxPrice="1000",
+                                       minArea="30",
+                                       maxArea="70")
+        self.assertEquals(EXPECTED_QUERRY_STRING, querry.asUrlString())
+        
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
+    # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
