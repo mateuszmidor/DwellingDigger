0c0b2e3012a1103fa4098146d55af494f258d9f7
mateusz <kontakt@mateuszmidor.com>
2015-01-20 00:30:40 +0100

    Renamed offer_querry.py to offer_search_querry.py and offer_querry_test.py to offer_search_querry_test.py, minor refactoring in names and comments.

--- src/offers/gumtree/offer_search_querry.py
+++ src/offers/gumtree/offer_search_querry.py
@@ -5,25 +5,24 @@ Created on 18-01-2015
 '''
 import urllib
 
-class OfferQuerry(object):
-    '''
-    This class represents gumtree offer querry with offer filters.
-    It can be used to generate and fetch filtered offer list from gumtree server. 
-    '''
+class OfferSearchQuerry(object):
+    """
+    Querry for searching offers.
+    This class represents gumtree offer search querry with offer filters.
+    It can be used to querry Gumtree for offers - it composes an url address for Gumtree server 
+    """
 
-    '''
-    Http request template for querring offers from server
-    '''
+    """Http request template for querring offers from Gumtree"""
     __TEMPLATE = 'http://www.gumtree.pl/fp-mieszkania-i-domy-do-wynajecia/{_city}/{_whereabouts}c9008l3200208?A_AreaInMeters_max={_maxArea}&A_AreaInMeters_min={_minArea}&A_ForRentBy=ownr&A_NumberRooms={_numRooms}&AdType=2&isSearchForm=true&maxPrice={_maxPrice}&maxPriceBackend=200000&minPrice={_minPrice}&minPriceBackend=100000'
     
     @staticmethod
     def compose(city="", whereabouts="", numRooms="", minPrice="", maxPrice="", minArea="", maxArea=""):
-        return OfferQuerry(city, whereabouts, numRooms, minPrice, maxPrice, minArea, maxArea)
+        return OfferSearchQuerry(city, whereabouts, numRooms, minPrice, maxPrice, minArea, maxArea)
     
     def __init__(self, city, whereabouts, numRooms, minPrice, maxPrice, minArea, maxArea):
         # city is obligatory
         if (not city):
-            raise ValueError("city is obligatory to compose a valid querry")
+            raise ValueError("city is obligatory to compose a valid search querry")
         
         self.city = city
         self.whereabouts = whereabouts # eg "Kazimierz"
@@ -44,8 +43,8 @@ class OfferQuerry(object):
         if (numRooms == "1"):
             numRooms = "10" 
         
-        querry = OfferQuerry.__TEMPLATE.format(_city=self.city, _whereabouts=whereabouts, _numRooms=numRooms,
-                                               _minPrice=self.minPrice, _maxPrice=self.maxPrice, _minArea=self.minArea,
-                                               _maxArea=self.maxArea)
+        querry = OfferSearchQuerry.__TEMPLATE.format(_city=self.city, _whereabouts=whereabouts, _numRooms=numRooms,
+                                                _minPrice=self.minPrice, _maxPrice=self.maxPrice, _minArea=self.minArea,
+                                                _maxArea=self.maxArea)
         
         return querry
--- test/offers/gumtree/offer_search_querry_test.py
+++ test/offers/gumtree/offer_search_querry_test.py
@@ -4,15 +4,14 @@ Created on 18-01-2015
 @author: mateusz
 '''
 import unittest
-from offers.gumtree.offer_querry import OfferQuerry
+from offers.gumtree.offer_search_querry import OfferSearchQuerry
 
-
-class OfferQuerryTest(unittest.TestCase):
+class OfferSearchQuerryTest(unittest.TestCase):
 
 
     def test_raises_on_empty_city(self):
         try:
-            OfferQuerry.compose(city="")
+            querry = OfferSearchQuerry.compose(city="")  # @UnusedVariable
             self.fail("Empty 'city' param should cause ValueError exception")
         except ValueError:
             # valid case
@@ -22,13 +21,13 @@ class OfferQuerryTest(unittest.TestCase):
             
     def test_all_parameters_supplied(self):
         EXPECTED_QUERRY_STRING = 'http://www.gumtree.pl/fp-mieszkania-i-domy-do-wynajecia/Krakow/Nowa%20Huta/c9008l3200208?A_AreaInMeters_max=70&A_AreaInMeters_min=30&A_ForRentBy=ownr&A_NumberRooms=10&AdType=2&isSearchForm=true&maxPrice=1000&maxPriceBackend=200000&minPrice=500&minPriceBackend=100000'
-        querry = OfferQuerry.compose(city="Krakow",
-                                       whereabouts="Nowa Huta",
-                                       numRooms="1",
-                                       minPrice="500",
-                                       maxPrice="1000",
-                                       minArea="30",
-                                       maxArea="70")
+        querry = OfferSearchQuerry.compose(city="Krakow",
+                                           whereabouts="Nowa Huta",
+                                           numRooms="1",
+                                           minPrice="500",
+                                           maxPrice="1000",
+                                           minArea="30",
+                                           maxArea="70")
         self.assertEquals(EXPECTED_QUERRY_STRING, querry.as_url_string())
         
 if __name__ == "__main__":
