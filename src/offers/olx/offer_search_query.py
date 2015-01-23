784fc6bdd92af3c230c5a4b58df1b569b5be2d9d
mateusz <kontakt@mateuszmidor.com>
2015-01-23 18:14:28 +0100

    Refactored src/offers/olx/offer_search_query.py

--- src/offers/olx/offer_search_query.py
+++ src/offers/olx/offer_search_query.py
@@ -11,10 +11,8 @@ class OfferSearchQuery(object):
     Search criteria: desired city, whereabouts, num rooms, min and max price, min and max square meters.
     Can be turned into url and used to query OLX for offers matching the criteria.
     """
-
-    """Request url template for querying OLX for offers"""
-    __TEMPLATE = 'http://olx.pl/nieruchomosci/mieszkania/wynajem/{_city}/{_whereabouts}?search%5Bfilter_float_price%3Afrom%5D={_min_price}&search%5Bfilter_float_price%3Ato%5D={_max_price}&search%5Bfilter_enum_rooms%5D%5B0%5D={_num_rooms}&search%5Bfilter_float_m%3Afrom%5D={_min_area}&search%5Bfilter_float_m%3Ato%5D={_max_area}'
     
+    "OLX offer query url example:"
     'http://olx.pl/nieruchomosci/mieszkania/wynajem/krakow/q-kurdwan%C3%B3w/?search%5Bfilter_float_price%3Afrom%5D=800&search%5Bfilter_float_price%3Ato%5D=1600&search%5Bfilter_enum_rooms%5D%5B0%5D=two&search%5Bfilter_float_m%3Afrom%5D=40&search%5Bfilter_float_m%3Ato%5D=70'
     __OLX_QUERY_BASE_URL = 'http://olx.pl/nieruchomosci/mieszkania/wynajem/'
 
@@ -46,43 +44,54 @@ class OfferSearchQuery(object):
         whereabouts = urllib.quote(whereabouts)  # escape special characters like spaces etc
         return url + "q-" + whereabouts + "/" # eg. http://olx.pl/nieruchomosci/mieszkania/wynajem/krakow/q-kurdwanów/
         
+    def __add_arg(self, args, template, new_arg):
+        """ Method for building http request argument list """
+        
+        # Nothing to add
+        if (new_arg == ""):
+            return args
+        
+        # If we already have some arguments - add ampersand separator
+        if (args != ""): 
+            args += "&"
+        
+        # Add new argument to argument list and return
+        return args + template.format(new_arg)
+    
     def __add_num_rooms(self, args, num_rooms):
-        if (num_rooms == ""): return args 
-        if (args != ""): args += "&"
-        # encode room number
-        ROOM_NUMBERS = {"1" : "one",
+        TEMPLATE = "search%5Bfilter_enum_rooms%5D%5B0%5D={0}"
+        ROOM_NUMBERS = {""  : "",
+                        "1" : "one",
                         "2" : "two",
                         "3" : "three",
                         "4" : "four"}
-        num_rooms_string = ROOM_NUMBERS[num_rooms]
-        return args + "search[filter_enum_rooms][0]={0}".format(num_rooms_string)
+        num_rooms_str = ROOM_NUMBERS[num_rooms]
+        return self.__add_arg(args, TEMPLATE, num_rooms_str)
     
     def __add_min_price(self, args, min_price):
-        if (min_price == ""): return args
-        if (args != ""): args += "&"
-        return args + "search[filter_float_price%3Afrom]={0}".format(min_price)
+        TEMPLATE = "search%5Bfilter_float_price%3Afrom%5D={0}"
+        return self.__add_arg(args, TEMPLATE, min_price)
     
     def __add_max_price(self, args, max_price):
-        if (max_price == ""): return args
-        if (args != ""): args += "&"
-        return args + "search[filter_float_price%3Ato]={0}".format(max_price)
+        TEMPLATE = "search%5Bfilter_float_price%3Ato%5D={0}"
+        return self.__add_arg(args, TEMPLATE, max_price)
     
     def __add_min_area(self, args, min_area):
-        if (min_area == ""): return args
-        if (args != ""): args += "&"
-        return args + "search[filter_float_m%3Afrom]={0}".format(min_area)
+        TEMPLATE = "search%5Bfilter_float_m%3Afrom%5D={0}"
+        return self.__add_arg(args, TEMPLATE, min_area)    
     
     def __add_max_area(self, args, max_area):
-        if (max_area == ""): return args
-        if (args != ""): args += "&"
-        return args + "search[filter_float_m%3Ato]={0}".format(max_area)
-    
+        TEMPLATE = "search%5Bfilter_float_m%3Ato%5D={0}"
+        return self.__add_arg(args, TEMPLATE, max_area)    
     
     def as_url_string(self):
+        
+        # Build the base url
         url = OfferSearchQuery.__OLX_QUERY_BASE_URL
         url = self.__add_city(url, self.city)
         url = self.__add_whereabouts(url, self.whereabouts)
             
+        # Build the argument list
         args = ""
         args = self.__add_num_rooms(args, self.num_rooms)
         args = self.__add_min_price(args, self.min_price)
@@ -90,7 +99,7 @@ class OfferSearchQuery(object):
         args = self.__add_min_area(args, self.min_area)
         args = self.__add_max_area(args, self.max_area)
         
-        
+        # Build the full url from base url and argument list
         full_url = url
         if (args != ""): full_url += "?" + args
         
