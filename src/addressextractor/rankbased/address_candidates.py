8ec2b72f399ad394200d3dee3527a1843bdd7e36
mateusz <kontakt@mateuszmidor.com>
2015-02-03 23:00:57 +0100

    Added addres_candidates.py and its test

---
+++ src/addressextractor/rankbased/address_candidates.py
@@ -0,0 +1,13 @@
+'''
+Created on 03-02-2015
+
+@author: mateusz
+'''
+
+class AddressCandidates(list):
+    '''
+    List of address candidates extrated from source string.
+    '''
+
+    def sort_by_correctness_and_precision(self):
+        self.sort(lambda a, b: cmp(b.correctness_rank + b.precision_rank, a.correctness_rank + a.precision_rank))
