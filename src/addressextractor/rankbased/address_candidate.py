719d30a17d57380b66809c2731bef8c4002a2c28
mateusz <kontakt@mateuszmidor.com>
2015-02-03 22:40:05 +0100

    RankBasedAddressExtractor initiated: added extractor.py and address_candidate.py, data/cities.txt, data/krakow_districts.txt, data/krakow_streets.txt

---
+++ src/addressextractor/rankbased/address_candidate.py
@@ -0,0 +1,23 @@
+'''
+Created on 03-02-2015
+
+@author: mateusz
+'''
+
+class AddressCandidate(object):
+    '''
+    This class represents an extracted address candidate.
+    Extraction can yield many results, such as city name, district name, street name.
+    The address candidate can be ranked in two areas.
+    Street with number has highest precision_rank.
+    City has lowest precision_rank.
+    Based on context, the correctness_rank is evaluated, eg "ul. Wielicka 23" will have 
+    higher correctness_rank than just "wielicka" (based on keyword "ul.", number, capital letter)
+    
+    @author m.midor
+    '''
+    address = ""    # the address itself
+    source = ""     # string where the address was found
+    correctness_rank = 0    # how sure we are this address is correct?
+    precision_rank = 0      # how precise (valuable to us) this address is?
+        
---
+++ src/addressextractor/rankbased/extractor.py
@@ -0,0 +1,17 @@
+'''
+Created on 27-01-2015
+
+@author: mateusz
+'''
+
+class Extractor(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
