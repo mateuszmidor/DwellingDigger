5de8c0076b13a66a3649e95e424f5199b8f19dce
mateuszmidor <kontakt@mateuszmidor.com>
2015-02-05 14:48:37 +0100

    Added RankSuffix and its test.

---
+++ src/addressextractor/rankbased/rank_suffix.py
@@ -0,0 +1,22 @@
+'''
+Created on 5 lut 2015
+
+@author: m.midor
+'''
+import re
+
+class RankSuffix(object):
+    '''
+    This class ranks address candidate correctness +1 and precission +1 
+    based on having suffix number, eg. "wielicka 24b"
+    '''
+
+
+    def rank(self, address_candidates):
+        #  word space number optional_letter eg. "wielicka 23b"
+        PATTERN = r"\w+\s{0,3}\d{1,3}\w?\b"
+        
+        for candidate in address_candidates:
+            if re.search(PATTERN, candidate.address, re.IGNORECASE):
+                candidate.correctness_rank += 1
+                candidate.precision_rank += 1
