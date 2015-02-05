04832b666394c097770ac834c06414f3ce85b391
mateuszmidor <kontakt@mateuszmidor.com>
2015-02-05 14:34:00 +0100

    Added RankCapitalLetter and its test.

---
+++ src/addressextractor/rankbased/rank_capital_letter.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on 5 lut 2015
+
+@author: m.midor
+'''
+import re
+
+class RankCapitalLetter(object):
+    '''
+    This class ranks address candidate correctness probability based on 
+    whether the address starts with capital letter
+    '''
+
+    def rank(self, address_candidates):
+        PATTERN = r"^[A-ZĄĆĘŁŃÓŚŹŻ]"
+        
+        for candidate in address_candidates:
+            if re.search(PATTERN, candidate.address):
+                candidate.correctness_rank += 1
+            
