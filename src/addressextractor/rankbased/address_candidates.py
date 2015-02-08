0af43f8b1aed0064fe7f60b3c7061edbf6122d37
mateusz <kontakt@mateuszmidor.com>
2015-02-08 13:42:55 +0100

    AddressCandidates can now __unicode__ itself

--- src/addressextractor/rankbased/address_candidates.py
+++ src/addressextractor/rankbased/address_candidates.py
@@ -8,6 +8,15 @@ class AddressCandidates(list):
     '''
     List of address candidates extrated from source string.
     '''
-
+    last_processed = None
+    
     def sort_by_correctness_precision(self):
         self.sort(lambda a, b: cmp(b.correctness_rank + b.precision_rank, a.correctness_rank + a.precision_rank))
+        AddressCandidates.last_processed = self
+        
+    def __unicode__(self):
+        r = u""
+        for candidate in self:
+            r += u"{0}[c:{1},p:{2}] ".format(candidate.address, candidate.correctness_rank, candidate.precision_rank)
+        
+        return r
