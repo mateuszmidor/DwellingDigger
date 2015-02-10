7b470a876793d33b029a02fbfd0ca2f76ee67a9d
mateuszmidor <kontakt@mateuszmidor.com>
2015-02-10 15:46:07 +0100

    Moved geocoder to src/outerspace as it facades a service located on the web and so cannot be reasonably tested.

---
+++ src/outerspaceaccess/geocoder.py
@@ -0,0 +1,15 @@
+'''
+Created on 10 lut 2015
+
+@author: m.midor
+'''
+
+class Geocoder(object):
+    '''
+    This class allows you to turn address string like "Wielicka 32, Kraków, Polska" to longitude, lattitude tuple
+    '''
+
+    @staticmethod
+    def geocode(address):
+        raise NotImplementedError()
+        
