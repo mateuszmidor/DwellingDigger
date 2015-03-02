c6297eaee74491fe7a5ff6b33be311db215fc41c
mateusz <kontakt@mateuszmidor.com>
2015-03-02 18:54:04 +0100

    Moved address validation from Dictionary to DictionaryEntry

--- src/mvc/model/addressextractor/rankbased/dictionary.py
+++ src/mvc/model/addressextractor/rankbased/dictionary.py
@@ -9,7 +9,7 @@ from src.mvc.model.addressextractor.rankbased.dictionary_entry import Dictionary
 class Dictionary(object):
     '''
     This class represents a dictionary of known addresses.
-    It allows to load the addresses from file.
+    It allows to load the addresses from a file.
     '''
     
     STREET      = DictionaryEntry.STREET
@@ -17,50 +17,55 @@ class Dictionary(object):
     CITY        = DictionaryEntry.CITY
     
     # Storage for dictionary contents (DictionaryEntry list)
-    __keys = None
+    __entries = None
+
 
     @staticmethod
     def from_file(filename, address_type, filereader = TextFileReader):
         dictionary = Dictionary()
         for address in filereader.read_lines(filename):
-            stripped_address = address.strip("\t\n\r ")
-            if not stripped_address:
-                continue
-            entry = DictionaryEntry(stripped_address, stripped_address, address_type)
+            entry = DictionaryEntry(address, address, address_type)
             dictionary.append(entry)
+            
         return dictionary
 
 
     def __init__(self, entries = []):
-        self.__keys = list()
-        self.extend(entries)
+        self.__entries = list()
+        map(self.append, entries)
 
 
     def extend(self, entries):
-        self.__keys.extend(entries)
+        map(self.append, entries)
              
 
     def append(self, entry):
-        if entry.name != u"" and entry.original_form != u"" and entry.address_type in [DictionaryEntry.CITY, DictionaryEntry.DISTRICT, DictionaryEntry.STREET]:
-            self.__keys.append(entry)
+        if entry.is_valid():
+            self.__entries.append(entry)
         
         
     def sort_longest_first(self):
         # longest first - for 'find' to match Krakowska before Krakow
         longToShort = lambda s1, s2: cmp(len(s2.name), len(s1.name))
-        self.__keys.sort(longToShort)
+        self.__entries.sort(longToShort)
                 
         
     def __iter__(self):
         ''' Provide iterator interface '''
-        return self.__keys.__iter__()
+        
+        return self.__entries.__iter__()
+    
+    
+    def __getitem__(self, index):
+        ''' Provide indexing interface '''
+        return self.__entries.__getitem__(index)
     
     
     def __contains__(self, key):
         ''' Provide "KEY in COLLECTION" interface '''
-        return self.__keys.__contains__(key)
+        return self.__entries.__contains__(key)
     
     
     def __len__(self):
         ''' Provide len interface '''
-        return self.__keys.__len__()
+        return self.__entries.__len__()
--- src/mvc/model/addressextractor/rankbased/dictionary_entry.py
+++ src/mvc/model/addressextractor/rankbased/dictionary_entry.py
@@ -6,17 +6,17 @@ Created on 28-02-2015
 
 class DictionaryEntry(object):
     '''
-    Represents single entry in address dictionary
+    Represents single entry in address dictionary.
+    Enforces the address name and original form to be in proper form.
     '''
 
     
     CITY        = 0
     DISTRICT    = 1
     STREET      = 2
-    UNKNOWN     = 99
     
     def __init__(self, name, original_form, address_type):
-        # Address name, possibly mutated, eg. Wielickiej
+        # Address name, possibly mutated, eg. wielickiej
         self.name = name.strip("\t\n\r ").lower()
         
         # Original form the name was derived from, eg. Wielicka
@@ -24,3 +24,8 @@ class DictionaryEntry(object):
         
         # Address type
         self.address_type = address_type
+        
+        
+    def is_valid(self):
+        """ Valid DictionaryEntry has name, original_form and address_type properly set """
+        return self.name and self.original_form and self.address_type in [DictionaryEntry.CITY, DictionaryEntry.DISTRICT, DictionaryEntry.STREET]
