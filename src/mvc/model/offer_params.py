0e8ab24005ca9e6024390d3981b2845195a04295
mateusz <kontakt@mateuszmidor.com>
2015-03-05 19:44:14 +0100

    Added OfferParams and its test

---
+++ src/mvc/model/offer_params.py
@@ -0,0 +1,104 @@
+'''
+Created on 05-03-2015
+
+@author: mateusz
+'''
+
+class OfferParams(object):
+    '''
+    This class represents params criteria provided by user to look for offers
+    '''
+
+
+    def __init__(self,
+                 city,
+                 whereabouts, 
+                 num_rooms, 
+                 min_price, 
+                 max_price, 
+                 min_area, 
+                 max_area):
+        
+        if not city:
+            raise ValueError("City is obligatory")
+        
+        if self.invalid_range(min_price, max_price):
+            raise ValueError("__max_price < __min_price")
+
+        if self.invalid_range(min_area, max_area):
+            raise ValueError("__max_area < __min_area")
+        
+        self.__city = city
+        self.__whereabouts = whereabouts
+        self.__num_rooms = num_rooms
+        self.__min_price = min_price
+        self.__max_price = max_price
+        self.__min_area = min_area
+        self.__max_area = max_area
+        
+    
+    def invalid_range(self, minimum, maximum):
+        if not minimum:
+            return False
+        
+        if not maximum:
+            return False
+        
+        if maximum < minimum:
+            return True
+        
+        
+    @staticmethod
+    def from_key_values(city=None,
+                        whereabouts=None, 
+                        num_rooms=None, 
+                        min_price=None, 
+                        max_price=None, 
+                        min_area=None, 
+                        max_area=None):
+        """ Use this method to build OfferParams from key-value pairs """
+        
+        return OfferParams(city, whereabouts, num_rooms, min_price, max_price, min_area, max_area)
+    
+    
+    @staticmethod
+    def from_cgi_fieldstorage(cgi_field_storage):
+        """ Use this method to build OfferParams from cgi.FieldStorage """
+        
+        s = cgi_field_storage
+        return OfferParams(s.getvalue("city", None),
+                           s.getvalue("whereabouts", None),
+                           s.getvalue("num_rooms", None),
+                           s.getvalue("min_price", None),
+                           s.getvalue("max_price", None),
+                           s.getvalue("min_area", None),
+                           s.getvalue("max_area", None))
+        
+        
+    def get_city(self):
+        return self.__city
+    
+    
+    def get_whereabouts(self):
+        return self.__whereabouts
+    
+
+    def get_num_rooms(self):
+        return self.__num_rooms
+    
+    
+    def get_min_price(self):
+        return self.__min_price
+    
+    
+    def get_max_price(self):
+        return self.__max_price
+    
+    
+    def get_min_area(self):
+        return self.__min_area
+    
+    
+    def get_max_area(self):
+        return self.__max_area 
+    
---
+++ test/mvc/model/offer_params_test.py
@@ -0,0 +1,124 @@
+'''
+Created on 05-03-2015
+
+@author: mateusz
+'''
+import unittest
+from src.mvc.model.offer_params import OfferParams
+
+class CgiFieldstorageStub:
+    """ This guy is used to test OfferParams.from_cgi_fieldstorage """
+    
+    @staticmethod
+    def getvalue(name, defvalue):
+        FIELDS = {"city"        : "Krakow",
+                  "whereabouts" : "Krowodrza", 
+                  "num_rooms"   : 4, 
+                  "min_price"   : 950, 
+                  "max_price"   : 1450, 
+                  "min_area"    : 45, 
+                  "max_area"    : 75}
+        
+        return FIELDS[name]
+    
+    
+class OfferParamsTest(unittest.TestCase):
+
+
+    def test_raises_on_empty_city1(self):
+        """ Tests empty city as empty string """
+        try:
+            params = OfferParams.from_key_values(city="")  # @UnusedVariable
+            self.fail("Empty 'city' param should cause ValueError exception")
+        except ValueError:
+            # valid case
+            pass
+
+
+    def test_raises_on_empty_city2(self):
+        """ Tests empty city as None """
+        try:
+            params = OfferParams.from_key_values(city=None)  # @UnusedVariable
+            self.fail("Empty 'city' param should cause ValueError exception")
+        except ValueError:
+            # valid case
+            pass
+      
+      
+    def test_raises_on_price_error(self):
+        """ Test if raises exception when min_price > max_price """
+        try:
+            params = OfferParams.from_key_values(city="Krakow", min_price = 1000, max_price = 950)  # @UnusedVariable
+            self.fail("min_price = 1000 and max_price = 950 should cause ValueError exception")
+        except ValueError:
+            # valid case
+            pass
+        
+        
+    def test_raises_on_area_error(self):
+        """ Test if raises exception when min_area > max_area """
+        try:
+            params = OfferParams.from_key_values(city="Krakow", min_area = 75, max_area = 45)  # @UnusedVariable
+            self.fail("min_area = 75 and max_area = 45 should cause ValueError exception")
+        except ValueError:
+            # valid case
+            pass
+        
+        
+    def test_from_key_values(self):
+        params = OfferParams.from_key_values(city="Krakow",
+                                             whereabouts="Krowodrza", 
+                                             num_rooms=4, 
+                                             min_price=950, 
+                                             max_price=1450, 
+                                             min_area=45, 
+                                             max_area=75)
+        
+        self.assertEqual(params.get_city(), "Krakow", 
+                         "city should be Krakow but is %s" % params.get_city())
+                            
+        self.assertEqual(params.get_whereabouts(), "Krowodrza", 
+                         "whereabouts should be Krowodrza but is %s" % params.get_whereabouts())
+        
+        self.assertEqual(params.get_num_rooms(), 4, 
+                         "num_rooms should be 4 but is %d" % params.get_num_rooms())
+        
+        self.assertEqual(params.get_min_price(), 950, 
+                         "min_price should be 950 but is %d" % params.get_min_price())   
+             
+        self.assertEqual(params.get_max_price(), 1450, 
+                         "max_price should be 1450 but is %d" % params.get_max_price()) 
+              
+        self.assertEqual(params.get_min_area(), 45, 
+                         "min_area should be 45 but is %d" % params.get_min_area()) 
+               
+        self.assertEqual(params.get_max_area(), 75, 
+                         "max_area should be 75 but is %d" % params.get_max_area())        
+                   
+  
+    def test_from_cgi_fieldstorage(self):
+        params = OfferParams.from_cgi_fieldstorage(CgiFieldstorageStub)
+        
+        self.assertEqual(params.get_city(), "Krakow", 
+                         "city should be Krakow but is %s" % params.get_city())
+                            
+        self.assertEqual(params.get_whereabouts(), "Krowodrza", 
+                         "whereabouts should be Krowodrza but is %s" % params.get_whereabouts())
+        
+        self.assertEqual(params.get_num_rooms(), 4, 
+                         "num_rooms should be 4 but is %d" % params.get_num_rooms())
+        
+        self.assertEqual(params.get_min_price(), 950, 
+                         "min_price should be 950 but is %d" % params.get_min_price())   
+             
+        self.assertEqual(params.get_max_price(), 1450, 
+                         "max_price should be 1450 but is %d" % params.get_max_price()) 
+              
+        self.assertEqual(params.get_min_area(), 45, 
+                         "min_area should be 45 but is %d" % params.get_min_area()) 
+               
+        self.assertEqual(params.get_max_area(), 75, 
+                         "max_area should be 75 but is %d" % params.get_max_area())                    
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
