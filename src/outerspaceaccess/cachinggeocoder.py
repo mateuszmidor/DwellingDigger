ef1697127f74fc8b22c5a866be14630dd9c830a8
mateuszmidor <kontakt@mateuszmidor.com>
2015-03-12 14:18:10 +0100

    Updated CachingGeocoder to use ExclusiveCacheFile instead of own solution with file locking.

--- src/outerspaceaccess/cachinggeocoder.py
+++ src/outerspaceaccess/cachinggeocoder.py
@@ -4,10 +4,7 @@ Created on 14-02-2015
 @author: mateusz
 '''
 from src.outerspaceaccess.geocoder import Geocoder
-from src.thirdparty.portalocker import portalocker
-from src.thirdparty.portalocker.utils import Lock
-import json
-import os.path
+from src.outerspaceaccess.exclusive_cache_file import ExclusiveCacheFile
 
 class CachingGeocoder(object):
     '''
@@ -27,7 +24,7 @@ class CachingGeocoder(object):
 
     def __del__(self):
         """ Update the cache file with new geocodings """
-        self.try_update_cache_file(self.__cache_filename)
+        self.try_update_cache_file(self.__cache_filename, self.__extending_cache)
         
         
     def try_read_cache_file(self, filename):
@@ -35,51 +32,22 @@ class CachingGeocoder(object):
         Reads cache as dict from file.
         If no file exists - returns empty dict.
         """
-        
-        # if no such file - return empty dict
-        if not os.path.isfile(filename):
-            print("File %s not exist. Returning empty cache dictionary" % filename)
-            return dict()
          
-        with open(filename, "r") as f:
-            # make sure no one writes to the cache file while we read
-            portalocker.lock(f, portalocker.LOCK_SH)
-            d = json.load(f)
-            print ("Num addresses read from cache file: %d" % len(d))
-            return d
-            # the file will be automatically closed and thus unlocked
+        d = ExclusiveCacheFile.read_or_empty(filename)
+        print ("Num addresses read from cache file: %d" % len(d))
+        return d
         
          
-    def try_update_cache_file(self, filename):
+    def try_update_cache_file(self, filename, update_dict):
         """ 
         Updates cache file with new geocodings.
         Gives up if can't open the cache file exclusively in 3 seconds.
         """
         # try for 3 seconds to exclusive lock the cache file
         FILE_LOCK_TIMEOUT = 3
+        print ("Num new addresses: %d" % len(update_dict))
+        ExclusiveCacheFile.new_or_update(filename, update_dict, FILE_LOCK_TIMEOUT)
         
-        print ("try update cache file started")
-        
-        # any new contents to update the cache file with?
-        if not self.__extending_cache:
-            print ("no new contents to update the cache file")
-            return
-        
-        # read the cache file to get the latest contents
-        cache = self.try_read_cache_file(filename)
-        cache.update(self.__extending_cache)
-        
-        print ("Num new geocodings: %d" % len(self.__extending_cache))
-            
-        # try lock and update
-        print("trying update the cache file..")
-        with Lock(filename, fail_when_locked=False, timeout=FILE_LOCK_TIMEOUT) as f:
-            json.dump(cache, f)
-            print("made it")
-            return
-        
-        print("couldnt exclusivel lock the file; cache file not updated")
-                   
 
     def geocode(self, address):
         if address in self.__cache:
