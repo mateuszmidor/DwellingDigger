d1f3b05fbfac041ba9cd5419c78449394f2a0832
mateuszmidor <kontakt@mateuszmidor.com>
2015-03-18 14:51:50 +0100

    DesktopMainView updated to take page template path from config and to self geocode city to-be-displayed,

--- src/mvc/view/desktop_main_view.py
+++ src/mvc/view/desktop_main_view.py
@@ -5,20 +5,31 @@ Created on 14-02-2015
 '''
 from src.mvc.view.google_map_points import GoogleMapPoints
 from src.mvc.view.lightwebframework.light_web_framework import LightWebFramework
+from src.outerspaceaccess.geocoder import Geocoder
+from src.ioc.dependency_injector import DependencyInjector, Inject
 
-MAP_CENTER_LONG = 50.0646501 
-MAP_CENTER_LATT = 19.9449799
+# this zoom should be correct for most of polish major cities like Krakow and Wroclaw
 MAP_ZOOM = 11
-TEMPLATE_HTML_FILENAME = u"DwellingDigger/data/DesktopView.htm"
-RESULT_HTML_FILENAME = u"DwellingDigger/DwellingMap.html"
 
+@DependencyInjector("config")
 class DesktopMainView(object):
 
+    config = Inject
+    
+    
+    def __init__(self, city):
+        "city - city in Poland to center the map on"
+        self.__city = city
+        
+        
     def show_offers(self, offers):
         points = GoogleMapPoints.from_offers(offers).as_java_script()
+        longitude, lattitude = Geocoder.geocode("%s, Polska" % self.__city)
+        
         FIELDS = {u"$POINTS$": points,
-                  u"$MAP_CENTER_LONG$": MAP_CENTER_LONG,
-                  u"$MAP_CENTER_LATT$": MAP_CENTER_LATT,
+                  u"$MAP_CENTER_LONG$": longitude,
+                  u"$MAP_CENTER_LATT$": lattitude,
                   u"$MAP_ZOOM$" : MAP_ZOOM}
                    
-        LightWebFramework.render_page_as_file(TEMPLATE_HTML_FILENAME, RESULT_HTML_FILENAME, FIELDS)  
+        desktop_template = DesktopMainView.config.get("PATHS", "desktopMainView")
+        LightWebFramework.render_page_as_file(desktop_template, "DwellingMap.html", FIELDS)  
