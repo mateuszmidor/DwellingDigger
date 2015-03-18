32724838ef35d735ff0dda037dc7cb344e5f3d48
mateuszmidor <kontakt@mateuszmidor.com>
2015-03-18 10:59:57 +0100

    Moved FileLogger and TerminalLogger into Logger.to_file and Logger.to_terminal. Now using dedicated logger so no rubbish from urllib3 is logged.

--- src/diagnostics/logger.py
+++ src/diagnostics/logger.py
@@ -6,9 +6,10 @@ Created on 17 mar 2015
 import logging
 from multiprocessing import Lock
 
+
 class Logger(object):
     '''
-    This class is a base logging class intended for extending.
+    This class allows for simple logging to file or to terminal.
     It is thread-safe.
     '''
     
@@ -19,32 +20,50 @@ class Logger(object):
     FATAL   = logging.FATAL
     
     
-    def __init__(self):
-        # the below is to be implemented in extending classes
-        # logging.basicConfig(filenamy=?, level=?)
+    @staticmethod
+    def to_terminal(log_level = WARN):  
+        handler = logging.StreamHandler()
+        handler.setLevel(log_level)
+        return Logger(handler)
+        
+ 
+    @staticmethod
+    def to_file(filename, log_level = WARN):  
+        handler = logging.FileHandler(filename)
+        handler.setLevel(log_level)
+        return Logger(handler)
+              
+          
+    def __init__(self, handler):
         self.__lock = Lock()
+        formatter = logging.Formatter('[%(asctime)s]  %(levelname)s:  %(message)s')
+        handler.setFormatter(formatter)
+        logger = logging.getLogger("MainLogger")
+        logger.setLevel(logging.DEBUG)
+        logger.addHandler(handler)
+        self.__logger = logger
         
         
     def debug(self, msg):
         with self.__lock:
-            logging.debug(msg)
+            self.__logger.debug(msg)
         
         
     def info(self, msg):
         with self.__lock:
-            logging.info(msg)
+            self.__logger.info(msg)
         
         
     def warn(self, msg):
         with self.__lock:
-            logging.warn(msg)
+            self.__logger.warn(msg)
         
         
     def error(self, msg):
         with self.__lock:
-            logging.error(msg)  
+            self.__logger.error(msg)  
             
             
     def exception(self, e):
         with self.__lock:
-            logging.exception(str(e))            
+            self.__logger.exception(str(e))            
