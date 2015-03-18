3f4da3f6e7ec4827123e9a42cf45c377a34af813
mateuszmidor <kontakt@mateuszmidor.com>
2015-03-18 13:46:08 +0100

    Application can now use file based configuration with ínjected "config" object.

--- src/ioc/initialize_dependency_injection.py
+++ src/ioc/initialize_dependency_injection.py
@@ -5,6 +5,7 @@ Created on 17 mar 2015
 '''
 from src.ioc.dependency_injector import DependencyInjector
 from src.diagnostics.logger import Logger
+from ConfigParser import ConfigParser
 
 """
 This module must be imported before all others;
@@ -14,5 +15,12 @@ before they are first time used.
 The configuration can be later reimplemented using some sort of config file.
 """
 
-# DependencyInjector.set_dependency("logger", Logger.to_file("DwellingDigger/diagnostics/logger.txt", Logger.DEBUG))
-DependencyInjector.set_dependency("logger", Logger.to_terminal(Logger.INFO))
+# Set the logger
+# logger = Logger.to_file("DwellingDigger/diagnostics/logger.txt", Logger.DEBUG)
+logger = Logger.to_terminal(Logger.INFO)
+DependencyInjector.set_dependency("logger", logger)
+
+# Set the config
+config = ConfigParser()
+config.read([r"DwellingDigger/config/config.ini"])
+DependencyInjector.set_dependency("config", config)
