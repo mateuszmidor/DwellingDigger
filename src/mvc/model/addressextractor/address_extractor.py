5b5c291fb3e901168e368152a4a0dc02138115d0
mateuszmidor <kontakt@mateuszmidor.com>
2015-03-18 14:52:39 +0100

    AddressExtractor updated to use the config.

--- src/mvc/model/addressextractor/address_extractor.py
+++ src/mvc/model/addressextractor/address_extractor.py
@@ -11,12 +11,18 @@ from src.mvc.model.addressextractor.rankbased.asciinator import Asciinator
 from src.mvc.model.addressextractor.rankbased.declinator import Declinator
 from src.mvc.model.addressextractor.rankbased.surnamenator import Surnamenator
 from src.mvc.model.addressextractor.rankbased.rank_based_extractor import RankBasedExtractor
+from src.ioc.dependency_injector import Inject, DependencyInjector
 
+
+@DependencyInjector("config")
 class AddressExtractor(object):
     '''
     This class is facade for addressextractor package.
     '''
 
+    config = Inject
+    
+    
     @staticmethod
     def rank_based(dictionary_file_type):
         """
@@ -36,8 +42,9 @@ class AddressExtractor(object):
         
     @staticmethod
     def for_krakow():
-        dicts = [("DwellingDigger/data/krakow_streets.txt", Dictionary.STREET), 
-                 ("DwellingDigger/data/krakow_districts.txt", Dictionary.DISTRICT)]
+        DATA_PATH = AddressExtractor.config.get("PATHS", "data")
+        dicts = [(DATA_PATH + "krakow_streets.txt", Dictionary.STREET), 
+                 (DATA_PATH + "krakow_districts.txt", Dictionary.DISTRICT)]
         return AddressExtractor.rank_based(dicts)
 
     @staticmethod
