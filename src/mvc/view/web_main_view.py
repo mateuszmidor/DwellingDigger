1231dee3a62bc5128bbd74a741a825811dcec583
mateuszmidor <kontakt@mateuszmidor.com>
2015-03-19 13:14:10 +0100

    Updated WebMainView to take name of city it is supposed to show on the map. Updated index.py to provide the city name to WebMainView. Updated config.ini to provide path to html template for WebMainView to render.

--- src/mvc/view/web_main_view.py
+++ src/mvc/view/web_main_view.py
@@ -5,20 +5,31 @@ Created on 04-03-2015
 '''
 from src.mvc.view.google_map_points import GoogleMapPoints
 from src.mvc.view.lightwebframework.light_web_framework import LightWebFramework
+from src.ioc.dependency_injector import DependencyInjector, Inject
+from src.outerspaceaccess.geocoder import Geocoder
 
-MAP_CENTER_LONG = 50.0646501 
-MAP_CENTER_LATT = 19.9449799
+# this zoom should be correct for most of polish major cities like Krakow and Wroclaw
 MAP_ZOOM = 11
-TEMPLATE_HTML_FILENAME = u"DwellingDigger/data/WebMainView.htm"
 
+@DependencyInjector("config")
 class WebMainView(object):
 
+    config = Inject
+    
+    
+    def __init__(self, city):
+        "city - city in Poland to center the map on"
+        self.__city = city
+        
+            
     def show_offers(self, offers):
         points = GoogleMapPoints.from_offers(offers).as_java_script()
+        longitude, lattitude = Geocoder.geocode("%s, Polska" % self.__city)
         FIELDS = {u"$POINTS$": points,
-                  u"$MAP_CENTER_LONG$": MAP_CENTER_LONG,
-                  u"$MAP_CENTER_LATT$": MAP_CENTER_LATT,
+                  u"$MAP_CENTER_LONG$": longitude,
+                  u"$MAP_CENTER_LATT$": lattitude,
                   u"$MAP_ZOOM$" : MAP_ZOOM}
                    
-        LightWebFramework.render_page_as_http_response(TEMPLATE_HTML_FILENAME, FIELDS) 
+        web_page__template = WebMainView.config.get("PATHS", "webMainView")                   
+        LightWebFramework.render_page_as_http_response(web_page__template, FIELDS) 
         
