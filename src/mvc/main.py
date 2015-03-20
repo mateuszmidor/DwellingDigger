5e6c107edb88460128fe18e92a9c4ec6857d5d73
mateuszmidor <kontakt@mateuszmidor.com>
2015-03-20 09:27:36 +0100

    Updated Main to take num worker threads and num presented offers values from config.

--- src/mvc/main.py
+++ src/mvc/main.py
@@ -4,15 +4,22 @@ Created on 27-02-2015
 @author: mateusz
 '''
 from src.mvc.model.offers import Offers
+from src.ioc.dependency_injector import DependencyInjector, Inject
 
+@DependencyInjector("config")
 class Main(object):
+    
+    config = Inject
+    
     '''
     DwellingDigger main page controller.
     '''
     @staticmethod
     def run(params, view):
+        num_offers = Main.config.getint("OUTPUT", "numPresentedOffers")
+        num_threads = Main.config.getint("PERFORMANCE", "numWorkerThreads")
         offers = Offers.get_from_all_sources(offer_params=params, 
-                                             max_offer_count=25, 
-                                             max_parallel_count=5)
+                                             max_offer_count=num_offers, 
+                                             max_parallel_count=num_threads)
         view.show_offers(offers)
         
