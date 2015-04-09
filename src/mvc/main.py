5ee58d7ebb154cae6a098b190c2aac30c46aff67
mateusz <kontakt@mateuszmidor.com>
2015-04-09 19:56:49 +0200

    Added loggin session start - session end in Main

--- src/mvc/main.py
+++ src/mvc/main.py
@@ -5,17 +5,21 @@ Created on 27-02-2015
 '''
 from src.mvc.model.offers import Offers
 from src.ioc.dependency_injector import DependencyInjector, Inject
+import time
 
-@DependencyInjector("config")
+@DependencyInjector("config", "logger")
 class Main(object):
     
     config = Inject
+    logger = Inject
     
     '''
     DwellingDigger main page controller.
     '''
     @staticmethod
     def run(params, view):
+        Main.logger.info("New session: " + time.strftime("%X, %x"))
+        
         num_offers = Main.config.getint("OUTPUT", "numPresentedOffers")
         num_threads = Main.config.getint("PERFORMANCE", "numWorkerThreads")
         offers = Offers.get_from_all_sources(offer_params=params, 
@@ -23,3 +27,4 @@ class Main(object):
                                              max_parallel_count=num_threads)
         view.show_offers_and_params(offers, params)
         
+        Main.logger.info("Session done. " + time.strftime("%X") + "\n")  
