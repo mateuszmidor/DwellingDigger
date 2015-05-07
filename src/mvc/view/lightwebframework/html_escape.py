7f968247b994b5572db1e7a4fe6cee4c22638354
mateusz <kontakt@mateuszmidor.com>
2015-05-07 18:42:57 +0200

    Added HtmlEscape and its test

---
+++ src/mvc/view/lightwebframework/html_escape.py
@@ -0,0 +1,26 @@
+'''
+Created on 06-05-2015
+
+@author: mateusz
+'''
+
+class HtmlEscape(object):
+    '''
+    This class escapes special characters to HTML equivalents
+    '''
+
+
+    @staticmethod
+    def escape(text):
+        print "escaping", text
+        html_escape_table = {
+                               u"&": u"&amp;",
+                               u'"': u"&quot;",
+                               u"'": u"&apos;",
+                               u">": u"&gt;",
+                               u"<": u"&lt;",
+                               u"\n" : u"<br />",
+                               u"\r" : u"",
+                                } 
+        return u"".join(html_escape_table.get(c,c) for c in text)       
+        
---
+++ test/mvc/view/lightwebframework/html_escape_test.py
@@ -0,0 +1,71 @@
+'''
+Created on 07-05-2015
+
+@author: mateusz
+'''
+import unittest
+from src.mvc.view.lightwebframework.html_escape import HtmlEscape
+
+
+class HtmlEscapeTest(unittest.TestCase):
+
+
+    def test_escape_ampersand(self):
+        STRING = "Tom&Jerry"
+        EXPECTED = "Tom&amp;Jerry"
+        result = HtmlEscape.escape(STRING)
+        self.assertEqual(result, EXPECTED, 
+                         "Escaped string should be '%s' but was '%s'" % (EXPECTED, result))
+        
+        
+    def test_escape_quotation(self):
+        STRING = 'Really "fast" internet'
+        EXPECTED = 'Really &quot;fast&quot; internet'
+        result = HtmlEscape.escape(STRING)
+        self.assertEqual(result, EXPECTED, 
+                         "Escaped string should be '%s' but was '%s'" % (EXPECTED, result))
+        
+                
+    def test_escape_apostrophe(self):
+        STRING = "kill'em all"
+        EXPECTED = "kill&apos;em all"
+        result = HtmlEscape.escape(STRING)
+        self.assertEqual(result, EXPECTED, 
+                         "Escaped string should be '%s' but was '%s'" % (EXPECTED, result))
+
+
+    def test_escape_lessthan(self):
+        STRING = "4 < 5"
+        EXPECTED = "4 &lt; 5"
+        result = HtmlEscape.escape(STRING)
+        self.assertEqual(result, EXPECTED, 
+                         "Escaped string should be '%s' but was '%s'" % (EXPECTED, result))
+        
+        
+    def test_escape_greaterthan(self):
+        STRING = "11 > 7"
+        EXPECTED = "11 &gt; 7"
+        result = HtmlEscape.escape(STRING)
+        self.assertEqual(result, EXPECTED, 
+                         "Escaped string should be '%s' but was '%s'" % (EXPECTED, result))      
+        
+        
+    def test_escape_newline(self):
+        STRING = "line1\nline2"
+        EXPECTED = "line1<br />line2"
+        result = HtmlEscape.escape(STRING)
+        self.assertEqual(result, EXPECTED, 
+                         "Escaped string should be '%s' but was '%s'" % (EXPECTED, result))   
+           
+           
+    def test_escape_cr(self):
+        STRING = "line\rline"
+        EXPECTED = "lineline"
+        result = HtmlEscape.escape(STRING)
+        self.assertEqual(result, EXPECTED, 
+                         "Escaped string should be '%s' but was '%s'" % (EXPECTED, result))   
+        
+                          
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
