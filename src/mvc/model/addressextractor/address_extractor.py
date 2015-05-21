1396a80b78d380f25cd0de7afcfb1da90a71692e
mateuszmidor <kontakt@mateuszmidor.com>
2015-05-21 13:48:20 +0200

    Refactored AddressExtractor and its test.

--- src/mvc/model/addressextractor/address_extractor.py
+++ src/mvc/model/addressextractor/address_extractor.py
@@ -22,33 +22,53 @@ class AddressExtractor(object):
 
     config = Inject
     
-    
     @staticmethod
-    def rank_based(dictionary_file_type):
+    def __address_dict_for_city(city_name):
+        """ 
+        Creates a dictionary of streets and districts of given city.
+        The streets and districts are loaded from data files.
+        @param city_name: must correspond to proper filename in /data directory.     
         """
-        Builds and returns RankBasedExtractor.
-        """
+        # path to folder with files containing streets and districts of given city 
+        # see: config.ini->PATHS to find out what the "data" path really is
+        data_path = AddressExtractor.config.get("PATHS", "data")
         
+        # path to file containing streets, eg. data/krakow_streets.txt
+        streets_path = data_path + city_name + "_streets.txt"
+        
+        # path to file containing districts, eg. data/krakow_districts.txt
+        districts_path = data_path + city_name + "_districts.txt"
+        
+        # build the dictionary from streets and districts
         dictionary = Dictionary()
-        for filename, address_type in dictionary_file_type:
-            dictionary.extend(Dictionary.from_file(filename, address_type))
+        dictionary.extend(Dictionary.from_file(streets_path, Dictionary.STREET))
+        dictionary.extend(Dictionary.from_file(districts_path, Dictionary.DISTRICT))
         
+        # complement the dictionary with no-national-characters versions of streets and districts
         Asciinator.asciinate_dictionary(dictionary)
+        
+        # complement the dictionary with declinated ie. eg. "wielickiej" or "slonecznym" versions of streets and districts
         Declinator.declinate_dictionary(dictionary)
-        Surnamenator.surnamenate_dictionary(dictionary)
         
-        return RankBasedExtractor(dictionary)
-        
-    @staticmethod
-    def for_krakow():
-        DATA_PATH = AddressExtractor.config.get("PATHS", "data")
-        dicts = [(DATA_PATH + "krakow_streets.txt", Dictionary.STREET), 
-                 (DATA_PATH + "krakow_districts.txt", Dictionary.DISTRICT)]
-        return AddressExtractor.rank_based(dicts)
+        # complement the dictionary with shortened eg. "Dietla" instead of "Józefa Dietla" versions of streets and districts
+        Surnamenator.surnamenate_dictionary(dictionary)        
+        return dictionary
+      
 
     @staticmethod
-    def for_city(city):
-        if city.lower() in [u"krakow", u"kraków"]:
-            return AddressExtractor.for_krakow()
+    def for_city(city_name):
+        """
+        Creates an address extractor specialized for given city.
+        @param city_name: non-ascii, lowercase name of the city. Eg. "krakow"
+        """
+         
+        # prepare dictionary of streets and addresses that can be found in given city 
+        dictionary = None
         
-        raise NotImplementedError()
+        # only support the cities for which files with streets and districts are prepared
+        if city_name.lower() in [u"krakow", u"kraków"]:
+            dictionary = AddressExtractor.__address_dict_for_city("krakow")
+        else:
+            raise NotImplementedError("City %s not supported, yet" % city_name)
+        
+        return RankBasedExtractor(dictionary)
--- test/mvc/model/addressextractor/address_extractor_test.py
+++ test/mvc/model/addressextractor/address_extractor_test.py
@@ -7,6 +7,7 @@ import unittest
 from src.mvc.model.addressextractor.address_extractor import AddressExtractor
 from mock import patch, Mock, call
 from src.mvc.model.addressextractor.rankbased.dictionary import Dictionary
+from src.ioc.dependency_injector import DependencyInjector
 
 
 class AddressExtractorTest(unittest.TestCase):
@@ -16,7 +17,7 @@ class AddressExtractorTest(unittest.TestCase):
     @patch('src.mvc.model.addressextractor.address_extractor.Asciinator')
     @patch('src.mvc.model.addressextractor.address_extractor.Dictionary')
     @patch('src.mvc.model.addressextractor.address_extractor.RankBasedExtractor')
-    def test_rank_based(self, extractor_mock, dictionary_mock, asciinator_mock, declinator_mock, surnamenator_mock):
+    def test_for_city(self, extractor_mock, dictionary_mock, asciinator_mock, declinator_mock, surnamenator_mock):
         """ 
         Check if input dictionaries are properly prepared before providing them to RankBasedExtractor(dictionaries)
         """
@@ -25,9 +26,15 @@ class AddressExtractorTest(unittest.TestCase):
         # dictionary instances returned by "from_file" [strets, districts]
         d1 = Mock() 
         d2 = Mock()
+        config_mock = Mock()
+        config_mock.get = Mock(return_value = "")
         
         # Dictionary class 
         dictionary_mock.from_file = Mock(side_effect = [d1, d2])
+        # supply original constants to the mock as they are used by address_extractor 
+        dictionary_mock.STREET = Dictionary.STREET
+        dictionary_mock.DISTRICT = Dictionary.DISTRICT
+        
         # dictionary instance returned by constructor "Dictionary()"
         d3 = Mock()
         dictionary_mock.return_value = d3
@@ -38,16 +45,17 @@ class AddressExtractorTest(unittest.TestCase):
         surnamenator_mock.surnamenate_dictionary = Mock()
         
         # Run tested method
-        AddressExtractor.rank_based([("streets.dat", Dictionary.STREET),
-                                     ("districts.dat", Dictionary.DISTRICT)])
+        DependencyInjector.manual_inject("config", config_mock, AddressExtractor)
+        AddressExtractor.for_city("krakow")
         
         # Check the scenario properly played
         d3.assert_has_calls([call.extend(d1), call.extend(d2)])
         
         
         # any_order since dictionary_mock has also the constructor and extend() and sort_longest_first called
-        dictionary_mock.assert_has_calls([call.from_file("streets.dat", Dictionary.STREET), 
-                                          call.from_file("districts.dat", Dictionary.DISTRICT)],
+#         fail(dictionary_mock.method_calls)
+        dictionary_mock.assert_has_calls([call.from_file("krakow_streets.txt", Dictionary.STREET), 
+                                          call.from_file("krakow_districts.txt", Dictionary.DISTRICT)],
                                           any_order=True)
         asciinator_mock.asciinate_dictionary.assert_called_once_with(d3)
         declinator_mock.declinate_dictionary.assert_called_once_with(d3)
