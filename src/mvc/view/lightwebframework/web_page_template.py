5433183e177c80f7e845e34e3a8869919aed5df7
mateuszmidor <kontakt@mateuszmidor.com>
2015-05-28 13:45:39 +0200

    Refactored WebPageTemplate

--- src/mvc/view/lightwebframework/web_page_template.py
+++ src/mvc/view/lightwebframework/web_page_template.py
@@ -2,13 +2,16 @@
 Created on 31-07-2014
 
 @author: mateusz
-This class allows to load HTML template, 
-substitue values for predefined $fields$ and return resulting html.
 '''
 from src.outerspaceaccess.text_file_writer import TextFileWriter
 from src.outerspaceaccess.text_file_reader import TextFileReader
 
 class WebPageTemplate():
+    """ 
+    This class allows you to load HTML template, 
+    substitue predefined $fields$ and return resulting html/save it to file.
+    """
+        
     __html = u""
     
     @staticmethod
@@ -16,23 +19,30 @@ class WebPageTemplate():
         html = TextFileReader.read(filename)
         return WebPageTemplate(html)
         
+        
     @staticmethod
     def from_html_string(html):
         return WebPageTemplate(html)
     
+    
     def __init__(self, html):
         self.__html = html
         
+        
     def set_field(self, fieldname, value):
         if isinstance(value, str):
-            value = unicode(value, 'utf-8') # from string
+            # unicode the value if it is string from string
+            value = unicode(value, 'utf-8') 
         else:
-            value = unicode(value) # from eg float, integer, etc
+            # unicode the value other than str eg. float, integer, etc
+            value = unicode(value) 
             
         self.__html = self.__html.replace(unicode(fieldname), value)
         
+        
     def save_to_file(self, filename):
         TextFileWriter.write(filename, self.__html)
+        
         
     def get_html_string(self):
         return self.__html
